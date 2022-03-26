dd89d87573fa6918f03d8146570ac5f433567abb
aleksandr <aleksaandr24@gmail.com>
2022-03-26 21:23:01 +0500

    Form empty fields validations ended

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 "use strict";
 
+function sendForm() {
+    
+    let requiredFields = ['theme', 'e-mail', 'name', 'question'];
+    let form = document.forms[1];
+    let i, j, flag;
+
+    for (j = 0; j < requiredFields.length; j++) {
+        for (i = 0; i < form.length; i++) {
+            form.elements[i].style.outline = 'none';
+            form.elements[i].style.color = 'initial';
+        }
+    }
+
+    for (j = 0; j < requiredFields.length; j++) {
+        for (i = 0; i < form.length; i++) {
+            if ((form.elements[i].name === requiredFields[j]) && (form.elements[i].value === '')) {
+                form.elements[i].style.outline = '1px solid red';
+                form.elements[i].style.color = 'red';
+                flag = false;
+            }
+        }
+    }
+
+    return flag;
+}
